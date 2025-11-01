519643d66eb3a3a36837e83b11ad801cbe07ed25
[dyad] <jeffyta@HOTMAIL.COM>
2025-11-01 16:21:48 -0400

    [dyad] Reintroduzi a SessionContextProvider do Supabase para corrigir o problema do preview. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ChordRecognizer from "./pages/ChordRecognizer";
 import NotFound from "./pages/NotFound";
 import { Toaster } from "@/components/ui/sonner";
-// Removendo a importação incorreta de SessionContextProvider
 import { supabase } from "@/integrations/supabase/client";
+import { SessionContextProvider } from '@supabase/auth-ui-react'; // Importando SessionContextProvider
 
 const App = () => (
-  <Router>
-    {/* Removendo o uso incorreto de SessionContextProvider */}
-    <Routes>
-      <Route path="/" element={<ChordRecognizer />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-    <Toaster />
-  </Router>
+  <SessionContextProvider supabaseClient={supabase}> {/* Envolvendo o Router com SessionContextProvider */}
+    <Router>
+      <Routes>
+        <Route path="/" element={<ChordRecognizer />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Toaster />
+    </Router>
+  </SessionContextProvider>
 );
 
 export default App;
